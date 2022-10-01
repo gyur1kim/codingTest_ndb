8e96d5ad65746a368eb5188016b661e66166b3ae
gyur1kim <qri1114@gmail.com>
2022-10-01 17:48:19 +0900

    221001_DFS응용

--- part02/chapter05_DFS,BFS/10_음료수 얼려먹기.py
+++ part02/chapter05_DFS,BFS/10_음료수 얼려먹기.py
@@ -37,9 +37,9 @@ def DFS(x, y):
     stack.append((x, y))
 
     while stack:
-        i, j = stack.pop()
+        x, y = stack.pop()
         for d in range(4):
-            nx, ny = i+dx[d], j+dy[d]
+            nx, ny = x+dx[d], y+dy[d]
             if 0<=nx<N and 0<=ny<M and not visited[nx][ny] and iceList[nx][ny] == 0:
                 stack.append((nx, ny))
                 visited[nx][ny] = True
@@ -58,5 +58,4 @@ for i in range(N):
         if iceList[i][j] == 0 and not visited[i][j]:
             cnt += 1
             DFS(i, j)
-
 print(cnt)
