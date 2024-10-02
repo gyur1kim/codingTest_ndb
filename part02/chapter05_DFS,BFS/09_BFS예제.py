eafb278a565e9e204c23d90133f7c3c0a788f429
gyur1kim <qri1114@gmail.com>
2024-10-02 10:22:17 +0900

    :sparkles: BFS복습

--- part02/chapter05_DFS,BFS/09_BFS예제.py
+++ part02/chapter05_DFS,BFS/09_BFS예제.py
@@ -1,30 +1,28 @@
 from collections import deque
 
-def bfs(graph, start, visited):
-    queue = deque([start])
+def BFS(graph, start, visited):
+    queue = deque([start]);
     visited[start] = True
     while queue:
-        v = queue.popleft()
-        print(v, end=" ")
-        for i in graph[v]:
+        node = queue.popleft()
+        print(node, end=" ")
+        for i in graph[node]:
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
 
 
 graph = [
-    [],
-    [2, 3, 8],
-    [1, 7],
-    [1, 4, 5],
-    [3, 5],
-    [3, 4],
-    [7],
-    [2, 6, 8],
+    [], 
+    [2, 3, 8], 
+    [1, 7], 
+    [1, 4, 5], 
+    [3, 5], 
+    [3, 4], 
+    [7], 
+    [2, 6, 8], 
     [1, 7]
 ]
 
 visited = [False] * 9
-bfs(graph, 1, visited)
-
-# 결과 : 1 2 3 8 7 4 5 6
+BFS(graph, 1, visited)
