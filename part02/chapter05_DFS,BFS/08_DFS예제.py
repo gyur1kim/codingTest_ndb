5d3ca782c2761ca5c324405e6b4c1c8f78f7880e
gyur1kim <qri1114@gmail.com>
2024-10-02 10:13:29 +0900

    :sparkles: dfs 복습

--- part02/chapter05_DFS,BFS/08_DFS예제.py
+++ part02/chapter05_DFS,BFS/08_DFS예제.py
@@ -1,24 +1,21 @@
-def dfs(graph, v, visited):
-    visited[v] = True
-    print(v, end=" ")
-    for i in graph[v]:
+def DFS(graph, node, visited):
+    visited[node] = True
+    print(node, end=" ")
+    for i in graph[node]:
         if not visited[i]:
-            dfs(graph, i, visited)
-
+            DFS(graph, i, visited)
 
 graph = [
-    [],
-    [2, 3, 8],
-    [1, 7],
-    [1, 4, 5],
-    [3, 5],
-    [3, 4],
-    [7],
-    [2, 6, 8],
+    [], 
+    [2, 3, 8], 
+    [1, 7], 
+    [1, 4, 5], 
+    [3, 5], 
+    [3, 4], 
+    [7], 
+    [2, 6, 8], 
     [1, 7]
-]
+    ]
 
 visited = [False] * 9
-dfs(graph, 1, visited)
-
-# 결과 : 1 2 7 6 8 3 4 5
+DFS(graph, 1, visited)
