80260fbdca4a58ac6b813b750b31d57f9e0b54a1
gyur1kim <qri1114@gmail.com>
2024-10-04 02:56:54 +0900

    :sparkles: 다익스트라(힙큐) 복습

--- part02/chapter09_최단 경로/02_다익스트라(힙큐).py
+++ part02/chapter09_최단 경로/02_다익스트라(힙큐).py
@@ -24,47 +24,87 @@ import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
-# 노드, 간선
 n, v = map(int, input().split())
 start = int(input())
 
-# 그래프
 graph = [[] for _ in range(n + 1)]
 for _ in range(v):
-    nodeFrom, nodeTo, dist = map(int, input().split())
-    # 힙큐는 맨 앞에거 기준으로 뱉음
-    # 최단거리를 구하고 있으므로 dist를 앞에 두자
-    graph[nodeFrom].append((dist, nodeTo))
+    fromN, toN, dist = map(int, input().split())
+    graph[fromN].append((dist, toN))
 
-# 최단거리 관리 리스트
 distance = [INF] * (n + 1)
 
-# node는 (dist, to) 로 관리합니다 주의주의
 def dijkstra(start):
     q = []
     heapq.heappush(q, (0, start))
     distance[start] = 0
 
     while q:
-        print(q)
-        dist, current = heapq.heappop(q)
-        
-        # 방문한 적 없는 녀석만... 진행
-        if dist > distance[current]:
+        dist, currentNodeIdx = heapq.heappop(q)
+
+        if dist > distance[currentNodeIdx]:
             continue
 
-        # nexttNode = (dist, node)를 갖는다
-        for 다음 in graph[current]:
-            # 나를 거쳐가는 경우
-            cost = dist + 다음[0]
-            if cost < distance[다음[1]]:
-                distance[다음[1]] = cost
-                heapq.heappush(q, (cost, 다음[1]))
-
+        for nextNodeDist, nextNodeIdx in graph[currentNodeIdx]:
+            cost = distance[currentNodeIdx] + nextNodeDist
+            if cost < distance[nextNodeIdx]:
+                distance[nextNodeIdx] = cost
+                heapq.heappush(q, (cost, nextNodeIdx))
 
 dijkstra(start)
 
 for i in range(1, n+1):
-    if distance[i] == INF:
-        print('INFINITY')
-    else: print(distance[i])
+    print('INF' if distance[i] == INF else distance[i], end=" ")
+
+
+##########################################################
+# import sys
+# import heapq
+# input = sys.stdin.readline
+# INF = int(1e9)
+
+# # 노드, 간선
+# n, v = map(int, input().split())
+# start = int(input())
+
+# # 그래프
+# graph = [[] for _ in range(n + 1)]
+# for _ in range(v):
+#     nodeFrom, nodeTo, dist = map(int, input().split())
+#     # 힙큐는 맨 앞에거 기준으로 뱉음
+#     # 최단거리를 구하고 있으므로 dist를 앞에 두자
+#     graph[nodeFrom].append((dist, nodeTo))
+
+# # 최단거리 관리 리스트
+# distance = [INF] * (n + 1)
+
+# # node는 (dist, to) 로 관리합니다 주의주의
+# def dijkstra(start):
+#     q = []
+#     heapq.heappush(q, (0, start))
+#     distance[start] = 0
+
+#     while q:
+#         print(q)
+#         dist, current = heapq.heappop(q)
+        
+#         # 방문한 적 없는 녀석만... 진행
+#         if dist > distance[current]:
+#             continue
+
+#         # nexttNode = (dist, node)를 갖는다
+#         for 다음 in graph[current]:
+#             # 나를 거쳐가는 경우
+#             cost = dist + 다음[0]
+#             if cost < distance[다음[1]]:
+#                 distance[다음[1]] = cost
+#                 heapq.heappush(q, (cost, 다음[1]))
+
+
+# dijkstra(start)
+
+# for i in range(1, n+1):
+#     if distance[i] == INF:
+#         print('INFINITY')
+#     else: print(distance[i])
+##########################################################
