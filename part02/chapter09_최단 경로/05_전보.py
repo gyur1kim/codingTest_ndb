4c594a3d36d456f8030724d9e62756d44404d71e
gyur1kim <qri1114@gmail.com>
2024-10-05 01:36:08 +0900

    :sparkles: 전보 - 다익스트라
    - 출발점이 있으니까, 이런 경우는 다익스트라로 푸는게 나아용

--- part02/chapter09_최단 경로/05_전보.py
+++ part02/chapter09_최단 경로/05_전보.py
@@ -4,30 +4,77 @@
 1 3 2
 
 '''
+
+import heapq
+import sys
+
+input = sys.stdin.readline
 INF = int(1e9)
 
 # 도시 개수 n, 통로 개수 m, 메시지를 보내고자 하는 도시 c
 n, m, c = map(int, input().split())
 
-graph = [[INF] * (n+1) for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
 for _ in range(m):
     startCity, endCity, dist = map(int, input().split())
-    graph[startCity][endCity] = dist
+    graph[startCity].append((dist, endCity))
 
-for i in range(n+1):
-    graph[i][i] = INF
+distance = [INF] * (n+1)
 
-for k in range(1, n+1):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, city = heapq.heappop(q)
+        if dist > distance[city]: continue
+
+        for nextDist, nextCity in graph[city]:
+            cost = distance[city] + nextDist
+            if cost < distance[nextCity]:
+                distance[nextCity] = cost
+                heapq.heappush(q, (cost, nextCity))
+
+dijkstra(c)
 
 cities = 0
-time = -99999
-for i in range(1, n+1):
-    if graph[c][i] == INF or c == i: continue
+time = 0
+for dist in distance:
+    if dist == INF or dist == 0: continue
     cities += 1
-    if graph[c][i] > time:
-        time = graph[c][i]
+    time = max(time, dist)
 
 print(cities, time)
+
+
+
+################ 플로이드 워셜 ################
+# INF = int(1e9)
+
+# # 도시 개수 n, 통로 개수 m, 메시지를 보내고자 하는 도시 c
+# n, m, c = map(int, input().split())
+
+# graph = [[INF] * (n+1) for _ in range(n+1)]
+# for _ in range(m):
+#     startCity, endCity, dist = map(int, input().split())
+#     graph[startCity][endCity] = dist
+
+# for i in range(n+1):
+#     graph[i][i] = INF
+
+# for k in range(1, n+1):
+#     for i in range(1, n+1):
+#         for j in range(1, n+1):
+#             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+# cities = 0
+# time = -99999
+# for i in range(1, n+1):
+#     if graph[c][i] == INF or c == i: continue
+#     cities += 1
+#     if graph[c][i] > time:
+#         time = graph[c][i]
+
+# print(cities, time)
+##############################################
